52fbb1e7509d9fcf1ca94aaee8979bf38d24d71d
GWen124 <9708281@qq.com>
2022-01-16 03:17:40 +0800

    Update

--- Linux/agnoster.sh
+++ Linux/agnoster.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-curl  https://get.acme.sh | sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
