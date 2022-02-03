74abd787188b0e91bfe77a2820d37748079598e8
GWen124 <9708281@qq.com>
2022-02-04 03:08:34 +0800

    Update

--- Linux/portopen.sh
+++ Linux/portopen.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
- if [ $SYSTEM = "CentOS" ]; then
-        systemctl stop oracle-cloud-agent
-        systemctl disable oracle-cloud-agent
-        systemctl stop oracle-cloud-agent-updater
-        systemctl disable oracle-cloud-agent-updater
-        systemctl stop firewalld.service
-        systemctl disable firewalld.service
-        yellow "原生系统防火墙禁用成功"
-    else
-        iptables -P INPUT ACCEPT
-        iptables -P FORWARD ACCEPT
-        iptables -P OUTPUT ACCEPT
-        iptables -F
-        apt-get purge netfilter-persistent -y
-        yellow "原生系统防火墙禁用成功"
-    fi
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+apt-get purge netfilter-persistent -y
+yellow "原生系统防火墙禁用成功"
