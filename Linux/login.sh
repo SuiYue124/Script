1ef74d62b570bd57676d9d68c0b1a17e4e599a94
GWen124 <9708281@qq.com>
2022-02-06 18:39:25 +0800

    Update

---
+++ Linux/login.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# By GWen124
+# https://github.com/GWen124/Script/tree/master/Linux
+
+blue(){
+    echo -e "\033[34m\033[01m$1\033[0m"
+}
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+
+function login(){
+clear
+echo "                            "
+green "=================================================================================="
+echo "                            "
+yellow "1.我已使用Root账户登录"
+yellow "2.继续使用非Root账户登录"
+red "注意：非Root账户可能导致某些脚本不能运行！"
+echo "                           "
+red "0.退出脚本"
+echo "                            "
+green "=================================================================================="
+read -p "请输入选项:" loginNumberInput
+case "$loginNumberInput" in
+    1 ) 
+	[[ $(id -u) != 0 ]] && red "请使用“sudo -i”登录root用户后执行本脚本！！！" && exit 1
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/GWen124/Script/master/Linux/tools.sh)"
+	;;
+    2 ) 
+	[[ "$USER" == "root" ]] && red "请使用“su xxx”登录非root用户后执行本脚本！！！" && exit 1
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/GWen124/Script/master/Linux/tools.sh)"
+	;;
+	0 ) exit 1 ;;
+esac
+}
+
+login
