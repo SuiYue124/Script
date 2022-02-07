f01b7320358f77fa6d8cda2a3887e2c612fce565
GWen124 <9708281@qq.com>
2022-02-07 09:52:18 +0800

    Update

--- Linux/tools.sh
+++ Linux/tools.sh
@@ -544,9 +544,9 @@ function page5(){
 		7 ) QuickBox ;;
 		8 ) aria2 ;;
         0 ) menu
+		* ) echo "                            "
+		yellow "请输入正确的数字" 
     esac
-	echo "                            "
-	yellow "请输入正确的数字" 
 }
 menu
 
