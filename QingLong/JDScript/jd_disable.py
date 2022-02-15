8bc9273754fdae8ed671ca428313d3a9a1357763
GWen124 <9708281@qq.com>
2022-02-15 19:45:04 +0800

    Update

--- QingLong/JDScript/jd_disable.py
+++ QingLong/JDScript/jd_disable.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "GWen124_Script&GWen124_JDTest&raw_Telegram")
+sub_str = os.getenv("RES_SUB", "GWen124_Script_master&GWen124_JDTest_master&raw_Telegram")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
