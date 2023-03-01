b35c8bc46a530260eb26d217a5f998ff974fca23
GWen124 <9708281@qq.com>
2023-03-02 01:36:00 +0800

    Linux\tubecheck.sh

--- Linux/tubecheck.sh
+++ Linux/tubecheck.sh
@@ -2,7 +2,7 @@
 if [ "$(uname -m)" = "x86_64" ]; then
   wget -O tubecheck https://cdn.jsdelivr.net/gh/sjlleo/TubeCheck/CDN/tubecheck_1.0beta_linux_amd64 && chmod +x tubecheck && clear && ./tubecheck && rm tubecheck
 elif [ "$(uname -m)" = "aarch64" ]; then
-  wget -O tubecheck https://cdn.jsdelivr.net/gh/sjlleo/TubeCheck/CDN/tubecheck_1.0beta_linux_arm64 && chmod +x tubecheck && clear && ./tubecheck && rm tubecheck
+  wget -O tubecheck https://github.com/sjlleo/TubeCheck/releases/download/1.0Beta/tubecheck_1.0beta_linux_arm64 && chmod +x tubecheck && clear && ./tubecheck && rm tubecheck
 else
   echo "未知的系统架构"
 fi
