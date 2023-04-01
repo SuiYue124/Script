fb144b0e2427a37b33311bc4b766c54e328bcb4d
GWen124 <9708281@qq.com>
2023-04-01 18:52:58 +0800

    Linux\deldir.py

--- Linux/deldir.py
+++ Linux/deldir.py
@@ -1,27 +1,29 @@
-import os
+import shutil
 import time
 
 # 设定阈值为 122880MB，如不需要设定阈值，修改值为0。
 threshold = 122880
 
-# 指定要删除的文件路径和名称
-filename = "/path/to/your/file"
+# 指定要删除的目录路径
+dir_path = "/path/to/your/dir"
 
 # 设定默认延迟时间为 30 分钟，值为0时立即删除。
 default_delay_time_mins = 30
 default_delay_time_secs = default_delay_time_mins * 60
 
-# 获取指定目录的可用空间信息
-statvfs = os.statvfs(os.path.dirname(filename))
-free_space = statvfs.f_frsize * statvfs.f_bavail / (1024 ** 2)
+# 获取指定目录的使用空间信息
+total, used, free = shutil.disk_usage(dir_path)
 
-if free_space < threshold:
-    # 如果当前目录可用空间不足，则等待指定的延迟时间后再检查容量是否达到删除阈值
-    time.sleep(default_delay_time_secs)
-    statvfs = os.statvfs(os.path.dirname(filename))
-    free_space = statvfs.f_frsize * statvfs.f_bavail / (1024 ** 2)
-    if free_space < threshold:
-        os.remove(filename)
+if used < threshold:
+    # 如果当前目录使用空间未达到阈值，则退出脚本。
+    exit()
 else:
-    # 目录可用空间充足，直接删除指定文件
-    os.remove(filename)
+    # 如果当前目录使用空间已经超过阈值，则等待指定的延迟时间后再次检查使用空间是否已经超过阈值，
+    # 如果已经超过阈值且阈值不为0，则直接删除指定目录及其中的所有文件和子目录；否则立即删除指定目录及其中的所有文件和子目录。
+    if threshold != 0 and default_delay_time_secs > 0:
+        time.sleep(default_delay_time_secs)
+    total, used, free = shutil.disk_usage(dir_path)
+    if used >= threshold or threshold == 0:
+        shutil.rmtree(dir_path)
+    else:
+        exit()
