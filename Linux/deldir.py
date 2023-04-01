5a2edd7c4c525bb9641a00c22e13cb7758f8b7ed
GWen124 <9708281@qq.com>
2023-04-01 12:47:33 +0800

    Linux\deldir.py

---
+++ Linux/deldir.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+# 设定阈值为 122880MB
+threshold = 122880
+
+# 指定要删除的文件路径和名称
+filename = "/path/to/your/file"
+
+# 设定默认延迟时间为 30 分钟（单位：秒）
+default_delay_time_mins = 30
+default_delay_time_secs = default_delay_time_mins * 60
+
+# 获取指定目录的可用空间信息
+statvfs = os.statvfs(os.path.dirname(filename))
+free_space = statvfs.f_frsize * statvfs.f_bavail / (1024 ** 2)
+
+if free_space < threshold:
+    # 如果当前目录可用空间不足，则等待指定的延迟时间后再删除指定的文件
+    time.sleep(default_delay_time_secs)
+    os.remove(filename)
+else:
+    # 否则立即删除指定文件
+    os.remove(filename)
